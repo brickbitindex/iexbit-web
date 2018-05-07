cf5eb45df08bd21584588c423a3946a16d572a7d
xuxiaomeng <xuxiaomeng@baozou.com>
2018-05-07 21:31:39 +0800

    bugfix. remove fixed of balance.

--- src/market/components/order/input.jsx
+++ src/market/components/order/input.jsx
@@ -3,9 +3,10 @@ import classnames from 'classnames';
 import './input.scss';
 
 export default function OrderInput(props) {
+  const value = props.value || '';
   return (
     <div className={classnames('order-input', props.className)}>
-      <input className="tt" type="text" value={props.value} onChange={props.onChange} />
+      <input className="tt" type="text" value={value} onChange={props.onChange} />
       {props.suffix && (
         <span className="order-input-suffix light-text">{props.suffix}</span>
       )}
