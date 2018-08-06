b9d9734e464baeda5c2ab45fad900795a4636b45
heromen <15067179123@163.com>
2018-08-06 14:30:40 +0800

    feat:去除console

--- src/market/components/common/anonymousMask/index.jsx
+++ src/market/components/common/anonymousMask/index.jsx
@@ -3,7 +3,6 @@ import { FormattedMessage } from 'react-intl';
 
 import './style.scss';
 
-console.log(window.locale);
 function redirect(open) {
   window.location = `/${window.locale.toLowerCase()}?open=${open}&redirect_to=${encodeURIComponent(window.location.pathname + window.location.hash)}`;
 }
